2f129f6ee421428bdc41a40e58b3c4c5d6b30c3a
unknown <cogjava1778@iiht.tech>
2022-07-07 14:11:51 +0530

    changes in App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 
 import OrderForm from './orderform';
 import SpreadOperator from './spreadoperator';
+
 import CombineTwoObjects from './combiningtwoobjectsusingspread';
 
-function App() { //constructor function
+function App() {  //constructor function
   return (
    
    <div className="App">
