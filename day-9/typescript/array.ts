9115c542c9356a9aa37b13019af314484d5b202b
unknown <cogjava1778@iiht.tech>
2022-07-14 17:54:03 +0530

    typescript

---
+++ day-9/typescript/array.ts
@@ -0,0 +1,5 @@
+var alphas:string[];
+alphas = ["1","2","3","4"]
+console.log(alphas[0]);
+console.log(alphas[1]);
+
---
+++ day-9/typescript/method.ts
@@ -0,0 +1,19 @@
+//Class inheritance and Method Overriding
+
+class PrinterClass {
+    doPrint():void {
+        console.log("doPrint() from Parent called...");
+        
+    }
+}
+
+class StringPrinter extends PrinterClass {
+    doPrint():void {
+        super.doPrint()
+        console.log("doPrint() is printing a string...");      
+        
+    }
+}
+
+var obj = new StringPrinter()
+obj.doPrint()
